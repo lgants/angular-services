21f85c26d3391f8c8507a880fbc7c60290133047
lgants <lgants@gmail.com>
2017-07-30 17:51:25 -0500

    resolved issue with hierarchical injection

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 // 'provide' simply means, 'tell angular how to create it'
+// removed AccountsService from provider since that would create a new instance of the service, which is instantiated on a parent component and thereby passed down through heirarchical dependency injection
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -9,22 +11,28 @@ import { LoggingService } from '../logging.service';
   providers: [LoggingService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // no longer need to emit the event once the AccountsService service was added
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   // type is not optional in this case while creating a service
   // using private is a TS shortcut that adds the loggingService property (i.e. can access this.loggingService)
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // no longer need to emit the event once the AccountsService service was added
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
     // declaring the service as a variable will work, but doesn't conform with convention
     // const service = new LoggingService();
     // service.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
     this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus)
 
   }
 }
